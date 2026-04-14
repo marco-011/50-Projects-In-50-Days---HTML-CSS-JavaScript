55faad2a31ec1205d1604f6e9f8c8aa1ee6814cd
sanal R <sanalrattingal@gmail.com>
2026-04-14 16:47:10 +0530

    Update script.js

--- Day-31/script.js
+++ Day-31/script.js
@@ -1,1 +1,23 @@
 
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('click',
+    (e) => doTheTrick(e.target)
+))
+function doTheTrick(theClickedOne){
+    if(good.checked && cheap && fast.checked){
+        if(good === theClickedOne){
+            fast.checked = false
+        }
+
+        if(cheap === theClickedOne){
+            good.checked = false
+        }
+        if(fast === theClickedOne){
+            cheap.checked = false
+        }
+    }
+}
